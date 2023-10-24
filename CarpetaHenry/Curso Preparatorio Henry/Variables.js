748506d8978eaffa3e57c766f841f5eafb0450f3
RonaldArratia <ronaldarratia.999@gmail.com>
2023-10-24 09:44:19 -0400

    Primer ejercicio

--- CarpetaHenry/Curso Preparatorio Henry/Variables.js
+++ CarpetaHenry/Curso Preparatorio Henry/Variables.js
@@ -1,1 +1,31 @@
-var cajon; 
+// String
+
+var bootcamp = 'Henry'
+
+// Numeros
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 17.5;
+
+// Boleanos
+
+var estoyAburrido = true;
+
+var noEstoyAburrido = false;
+
+// Undefined
+
+var dato;
+console.log(dato);
+
+// Null
+
+var valorNull = null;
+
+// Metodo Length
+
+var palabra = 'Algo que decir';
+palabra.length
+
